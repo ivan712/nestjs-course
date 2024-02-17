aed9dc56bb7025c9b0c655c355f28b8a18ac658c
IvaN <nazarov.ivan.1992@list.ru>
2024-02-17 23:05:56 +0300

    fix

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,17 @@
-import { Body, Controller, HttpCode, Inject, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  Post,
+  UseInterceptors,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register.dto';
 import { LoginDto } from './dto/login.dto';
+import { ExceptionInterceptor } from '../exceptions/exception.interceptor';
 
 @Controller('auth')
+@UseInterceptors(ExceptionInterceptor)
 export class AuthController {
   constructor(private authService: AuthService) {}
 
--- src/exceptions/exception.interceptor.ts
+++ src/exceptions/exception.interceptor.ts
@@ -12,32 +12,31 @@ import { ExceptionCodes } from './codes';
 @Injectable()
 export class ExceptionInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    // next.handle() is an Observable of the controller's result value
     return next.handle().pipe(
       catchError((error) => {
-        if (error instanceof Exception) {
-          const code = error.getCode();
-          const message = error.message;
-
-          let status;
-          switch (code) {
-            case ExceptionCodes.BAD_REQUEST:
-              status = 400;
-              break;
-            case ExceptionCodes.NOT_FOUND:
-              status = 404;
-              break;
-            default:
-              status = 500;
-              break;
-          }
-          throw new HttpException(
-            { statusCode: status, message: { code, message } },
-            status,
-          );
-        } else {
+        if (!(error instanceof Exception)) {
           throw error;
         }
+
+        const code = error.getCode();
+        const message = error.message;
+
+        let status;
+        switch (code) {
+          case ExceptionCodes.BAD_REQUEST:
+            status = 400;
+            break;
+          case ExceptionCodes.NOT_FOUND:
+            status = 404;
+            break;
+          default:
+            status = 500;
+            break;
+        }
+        throw new HttpException(
+          { statusCode: status, message: { code, message } },
+          status,
+        );
       }),
     );
   }
